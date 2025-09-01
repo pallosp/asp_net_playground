b7699c1a33921d1c1227324038b4f2a10b7489c5
Péter Pallos <pallosp@gmail.com>
2025-09-01 17:32:05 +0200

    Factor out the weather model and controller

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using experimental_asp_net_webapi.Models;
+
+namespace experimental_asp_net_webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+  private static readonly string[] Summaries = new[]
+  {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  };
+
+  [HttpGet]
+  public IEnumerable<WeatherForecast> Get()
+  {
+    return Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast(
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            Summaries[Random.Shared.Next(Summaries.Length)]
+        ))
+        .ToArray();
+  }
+}
+
---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,7 @@
+namespace experimental_asp_net_webapi.Models;
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
