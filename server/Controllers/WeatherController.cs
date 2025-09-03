f05d637aec9e8cae551004f0ceec37c737d4d3d8
Péter Pallos <pallosp@gmail.com>
2025-09-03 10:48:02 +0200

    Validate the number of days

--- server/Controllers/WeatherController.cs
+++ server/Controllers/WeatherController.cs
@@ -14,15 +14,18 @@ public class WeatherController : ControllerBase
   };
 
   [HttpGet]
-  public IEnumerable<WeatherForecast> Get([FromQuery] int days = 5)
+  public ActionResult<IEnumerable<WeatherForecast>> Get([FromQuery] int days = 5)
   {
-    return Enumerable.Range(1, days).Select(index =>
+    if (days < 1 || days > 14)
+      return BadRequest("Invalid number of days");
+
+    var forecasts = Enumerable.Range(1, days).Select(index =>
         new WeatherForecast(
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             Summaries[Random.Shared.Next(Summaries.Length)]
         ))
         .ToArray();
+    return Ok(forecasts);
   }
 }
-
